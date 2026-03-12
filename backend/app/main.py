fb0ca01dd2e622869620488fed395f8f6d87807b
AnshMNSoni <anshsoni702@gmail.com>
2026-03-12 21:24:14 +0530

    core logic main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+
 from app.db.base import Base
 from app.db.session import engine
 from app.api.routes import url_routes
+from app.api.routes import redirect_routes
+from app.api.routes import analytics_routes
 
 app = FastAPI()
 
+@app.on_event("startup")
+async def startup():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
 
 app.include_router(url_routes.router)
+app.include_router(redirect_routes.router)
+app.include_router(analytics_routes.router)
